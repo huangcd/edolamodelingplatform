ee8413429cecd50f6e05d19311d82c7df822f63a
Huang Chongdi <huangcd.thu@gmail.com>
2011-12-20 09:40:27 +0800

    基本完成新建connector type的功能，调整CompoundEditor和AtomicEditor的画板样式

--- src/cn/edu/tsinghua/thss/tsmart/baseline/BaseLine.java
+++ src/cn/edu/tsinghua/thss/tsmart/baseline/BaseLine.java
@@ -3,7 +3,19 @@ package cn.edu.tsinghua.thss.tsmart.baseline;
 import java.util.Iterator;
 
 public interface BaseLine {
+    /**
+     * 通过用户选择的方式得到规则的迭代器
+     * 
+     * @return
+     */
     Iterator<Rule> getRules();
 
+    /**
+     * 给定基准线库名称和检索条件，返回相应的实体名
+     * 
+     * @param baseLineLib
+     * @param searchCondition
+     * @return
+     */
     String getEntity(String baseLineLib, String searchCondition);
 }
